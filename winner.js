dc553f03892b5a13910df17b74dcc6ac410ddf4e
ekbostan <114015851+ekbostan@users.noreply.github.com>
2023-04-12 16:06:14 -0700

    Update and rename public/winner.js to winner.js

--- winner.js
+++ winner.js
@@ -3,22 +3,13 @@ sendGetRequest("/getWinner")
 let winner_url = Winner_video.url;
 console.log(winner_url);
 
-// when this page is opened, get the most recently added video and show it.
-// function is defined in video.js
 let divElmt = document.getElementById("tiktokDiv");
 
 let reloadButton = document.getElementById("reload");
-// set up button to reload video in "tiktokDiv"
 reloadButton.addEventListener("click",function () {
   reloadVideo(tiktokDiv);
 });
 
-
-
-// always shows the same hard-coded video.  You'll need to get the server to 
-// compute the winner, by sending a 
-// GET request to /getWinner,
-// and send the result back in the HTTP response.
 console.log(winner_url, "THIS IS THE URL");
 showWinningVideo(winner_url);
 document.getElementById("winner1").textContent = "The winner is " + Winner_video.nickname;
